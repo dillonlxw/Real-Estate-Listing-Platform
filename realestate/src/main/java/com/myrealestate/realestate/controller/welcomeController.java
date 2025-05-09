49735aadbbd5ee12780457ab5d4850c8b19a16eb
dillonlxw <dillonlxw@gmail.com>
2025-05-09 16:46:51 +0800

    Fully completed welcome page

---
+++ realestate/src/main/java/com/myrealestate/realestate/controller/welcomeController.java
@@ -0,0 +1,16 @@
+package com.myrealestate.realestate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class welcomeController {
+
+    @GetMapping("/welcome")
+    public String welcome(Model model) {
+        model.addAttribute("welcomeMessage", "Welcome");
+        model.addAttribute("welcomeText", "Welcome to our Spring Real Estate Portal—your first step toward discovering the perfect property.\n Explore, connect, and get inspired as you embark on your home journey."); 
+        return "welcome";
+    }
+}
