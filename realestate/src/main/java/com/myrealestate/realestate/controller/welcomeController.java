79a22a6341c3815a4a5b6d9c4c53222475b37226
dillonlxw <dillonlxw@gmail.com>
2025-05-10 00:51:34 +0800

    Fixed minor issues in welcome page

--- realestate/src/main/java/com/myrealestate/realestate/controller/welcomeController.java
+++ realestate/src/main/java/com/myrealestate/realestate/controller/welcomeController.java
@@ -10,7 +10,7 @@ public class welcomeController {
     @GetMapping("/welcome")
     public String welcome(Model model) {
         model.addAttribute("welcomeMessage", "Welcome");
-        model.addAttribute("welcomeText", "Welcome to our Spring Real Estate Portal—your first step toward discovering the perfect property.\n Explore, connect, and get inspired as you embark on your home journey."); 
+        model.addAttribute("welcomeText", "Welcome to our Spring Real Estate Portal—your first step toward discovering the perfect property." + "\n\nExplore, connect, and get inspired as you embark on your home journey."); 
         return "welcome";
     }
 }
