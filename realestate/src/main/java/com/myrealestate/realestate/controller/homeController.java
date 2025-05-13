e0cf4ae9114f88ab52e39af7eb489eca911396e7
dillonlxw <dillonlxw@gmail.com>
2025-05-13 09:30:17 +0800

    Modified all welcome page and controls as home

--- realestate/src/main/java/com/myrealestate/realestate/controller/homeController.java
+++ realestate/src/main/java/com/myrealestate/realestate/controller/homeController.java
@@ -5,12 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class welcomeController {
+public class homeController {
 
-    @GetMapping("/welcome")
+    @GetMapping("/home")
     public String welcome(Model model) {
         model.addAttribute("welcomeMessage", "Welcome");
         model.addAttribute("welcomeText", "Welcome to our Spring Real Estate Portal—your first step toward discovering the perfect property." + "\n\nExplore, connect, and get inspired as you embark on your home journey."); 
-        return "welcome";
+        return "home";
     }
 }
