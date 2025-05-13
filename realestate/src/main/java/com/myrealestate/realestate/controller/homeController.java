cd4ff855bde8d61d257d46940c69c884c81171e5
dillonlxw <dillonlxw@gmail.com>
2025-05-13 10:25:47 +0800

    Minor changes to home web link

--- realestate/src/main/java/com/myrealestate/realestate/controller/homeController.java
+++ realestate/src/main/java/com/myrealestate/realestate/controller/homeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class homeController {
 
-    @GetMapping("/home")
+    @GetMapping("")
     public String welcome(Model model) {
         model.addAttribute("welcomeMessage", "Welcome");
         model.addAttribute("welcomeText", "Welcome to our Spring Real Estate Portal—your first step toward discovering the perfect property." + "\n\nExplore, connect, and get inspired as you embark on your home journey."); 
